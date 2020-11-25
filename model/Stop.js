73e587b82f597cf5aee0f4810a7076ec43bf4d72
charbel <charbel-boumaroun@outlook.com>
2020-11-25 09:31:05 +0200

    Fixed stops

--- model/Stop.js
+++ model/Stop.js
@@ -1,13 +1,12 @@
 class Stop {
 
-    constructor(stopId,stopDestination,stopGoogleURL,stopPictureURL,stopArrivalDate,stopDepartureDate,stopRank) {
+    constructor(stopId,stopDestination,stopGoogleURL,stopPictureURL,stopDuration,stopRank) {
       
       this.stopId = stopId;
       this.stopDestination = stopDestination;
       this.stopGoogleURL = stopGoogleURL;
       this.stopPictureURL = stopPictureURL;
-      this.stopArrivalDate = stopArrivalDate;
-      this.stopDepartureDate = stopDepartureDate;
+      this.stopDuration = stopDuration;
       this.stopRank = stopRank;
   
   
